f4edc20b93eea3563741c86e8ff62fdeb42a0112
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-31 05:00:42 +0000

    added more functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,25 @@ class ApplicationController < ActionController::Base
   end
 
   def forex
+    # change this to be from the api
+    # pull first value
     @array_of_numbers = Array.new
-    
+
     5.times do
       another_number = rand(1...100)
       
       @array_of_numbers.push(another_number)
     end
 
+    # pass first value to the html
     render({ :template => "conversion_template/forex.html.erb" })
+  end
+
+  def convert_first
+    # change this to be from the api
+    # pull first value and second to convert to
+    # make two arrays
+    # pass them to the html
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "application", :action => "home" })
   get("/forex", { :controller => "application", :action => "forex" })
-  # get("/:first_currency/", { :controller => "application", :action => "convert" })
-  # get("/:first_currency/:second_currency", { :controller => "application", :action => "convert" })
+  # get("/:first_currency/", { :controller => "application", :action => "convert_first" })
+  # get("/:first_currency/:second_currency", { :controller => "application", :action => "convert_first_to_second" })
 end
