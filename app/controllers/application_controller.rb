94ec2eb4c66dfe0e18a9b53c13d729eb208dee4d
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-31 04:55:13 +0000

    made forex page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  def home
+    render({ :template => "conversion_template/home.html.erb" })
+  end
+
+  def forex
+    @array_of_numbers = Array.new
+    
+    5.times do
+      another_number = rand(1...100)
+      
+      @array_of_numbers.push(another_number)
+    end
+
+    render({ :template => "conversion_template/forex.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", { :controller => "application", :action => "home" })
+  get("/forex", { :controller => "application", :action => "forex" })
+  # get("/:first_currency/", { :controller => "application", :action => "convert" })
+  # get("/:first_currency/:second_currency", { :controller => "application", :action => "convert" })
 end
